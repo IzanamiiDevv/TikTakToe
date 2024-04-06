ddffbaad7fe27bfbe5e0bd988efc9e7b30429075
IzanamiiDevv <rafaeloli596@gmail.com>
2024-04-06 16:56:48 +0800

    fonts

---
+++ src/components/Menu.tsx
@@ -0,0 +1,10 @@
+import './../assets/Menu.css';
+import './../assets/button.css';
+
+export default function Menu() {
+    return(
+        <div className="Menu">
+            <h1>TicTacToe</h1>
+        </div>
+    )
+}
