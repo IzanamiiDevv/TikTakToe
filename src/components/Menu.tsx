4a9b1a5308b97cfb2754dabdce9f6201efd3a5dd
IzanamiiDevv <rafaeloli596@gmail.com>
2024-04-06 17:04:55 +0800

    Homepage

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,6 +5,33 @@ export default function Menu() {
     return(
         <div className="Menu">
             <h1>TicTacToe</h1>
+            <button className='button' onClick={()=>{
+
+            }}>
+                <div className="top">Start</div>
+                <div className="bottom"></div>
+            </button>
+            <br />
+            <button className='button' onClick={()=>{
+                
+            }}>
+                <div className="top">Multiplayer</div>
+                <div className="bottom"></div>
+            </button>
+            <br />
+            <button className='button' onClick={()=>{
+                
+            }}>
+                <div className="top">Settings</div>
+                <div className="bottom"></div>
+            </button>
+            <br />
+            <button className='button'  onClick={()=>{
+                
+            }}>
+                <div className="top">Exit</div>
+                <div className="bottom"></div>
+            </button>
         </div>
     )
 }
